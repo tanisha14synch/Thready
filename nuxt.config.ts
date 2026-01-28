f84e9c5c8b4e5fe52370baa17ebb70a49d119926
Tanisha <tanisha@Tanishas-MacBook-Pro.local>
2026-01-28 17:50:23 +0530

    Remove Prisma from frontend package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import tailwindcss from "@tailwindcss/vite"; export default defineNuxtConfig({   modules: ['@pinia/nuxt', 'pinia-plugin-persistedstate/nuxt', '@nuxt/image'] , compatibilityDate: "2025-07-15", devtools: { enabled: true }, css: ['~/assets/css/main.css','@fortawesome/fontawesome-free/css/all.min.css'], vite: { plugins: [tailwindcss(),], }, });
+import tailwindcss from "@tailwindcss/vite"
+
+export default defineNuxtConfig({
+  modules: [
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate/nuxt',
+    '@nuxt/image'
+  ],
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: [
+    '~/assets/css/main.css',
+    '@fortawesome/fontawesome-free/css/all.min.css'
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  // Dev server configuration
+  devServer: {
+    port: 3000,
+    host: 'localhost'
+  }
+})
